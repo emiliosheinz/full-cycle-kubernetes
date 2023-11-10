1c5758e2dc336b52cfa57317a6f9f542cdaa3800
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-11-10 20:16:24 -0300

    feat: :sparkles: add config map

--- server.go
+++ server.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config-map-fruits", ConfigMapFruits)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s and I'm %s years old", name, age)
+}
+
+func ConfigMapFruits(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/config/fruits.txt")
+	if err != nil {
+		log.Fatal("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "Config Map Fruits: %s", string(data))
 }
