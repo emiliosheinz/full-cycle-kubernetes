b5f002fd59921954a17e97555db065b1f64c5f72
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-11-10 20:26:59 -0300

    feat: :sparkles: adds secrets

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/config-map-fruits", ConfigMapFruits)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":80", nil)
 }
 
@@ -26,4 +27,10 @@ func ConfigMapFruits(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Error reading file: ", err)
 	}
 	fmt.Fprintf(w, "Config Map Fruits: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
 }
